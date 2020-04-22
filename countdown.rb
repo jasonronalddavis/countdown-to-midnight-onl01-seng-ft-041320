5122c405ef1779def327f1bd1c7997bb62d455ab
Jason Davis <jdavis@c.ringling.edu>
2020-04-22 03:34:27 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -3,9 +3,10 @@ number = 10
 while number >0
   number -=1
   return "HAPPY NEW YEAR!"
+  puts "#{number} SECOND(S)!"
 end
 end
-puts "#{number} SECOND(S)!"
+
 
 #def countdown_with_sleep(number)
   #while number < 10
