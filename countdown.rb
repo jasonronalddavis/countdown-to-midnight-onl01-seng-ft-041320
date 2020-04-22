a9cd9cfc36c4b3854fed034df826446e45374fb9
Jason Davis <jdavis@c.ringling.edu>
2020-04-22 02:13:12 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,5 +1,14 @@
 def countdown
+while number < 10
 "#{number} SECOND(S)!"
 return "HAPPY NEW YEAR!"
 countdown_with_sleep
+end
+
+def using_while
+  levitation_force = 6
+  while levitation_force < 10
+	levitation_force +=1
+  puts "Wingardium Leviosa"
+end
 end
