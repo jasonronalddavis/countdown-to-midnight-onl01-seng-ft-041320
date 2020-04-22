bb48c1e88516e5b955162da7258b1db785e6e0e4
Jason Davis <jdavis@c.ringling.edu>
2020-04-22 04:12:31 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -9,7 +9,10 @@ end
 
 
 def countdown_with_sleep(number)
-  while number < 10
- number +=1
+  while number > 0
+    puts "#{number} SECOND(S)!"
+    sleep(1)
+ number -=1
 end
+"HAPPY NEW YEAR!"
 end
